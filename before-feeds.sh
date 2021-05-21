194a0e6476762daa83cf6844261a3e72639e4726
ArArgon <liaozping@gmail.com>
2021-05-21 13:48:49 +0800

    New configuration

--- before-feeds.sh
+++ before-feeds.sh
@@ -6,12 +6,17 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
+# File name: before-feeds.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# luci-app-adguardhome fix
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome package/luci-app-adguardhome
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/adguardhome	package/adguardhome
+svn co https://github.com/Lienol/openwrt-packages/trunk/devel/packr package/packr
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
